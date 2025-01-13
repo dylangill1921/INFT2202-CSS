330ef3d0d8951022f01b7709f3ca93989a07204f
dylangill1921 <156254542+dylangill1921@users.noreply.github.com>
2025-01-13 18:05:27 -0500

    week1 wd-css uploaded

---
+++ week1/scripts/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//IFE -Immediately Invoked Functional Expression
+(function() {
+    function Start()
+    {
+        console.log("Starting...");
+    }window.addEventListener("Load", Start);
+})();
