4f20867ba5fa46c7b3eb03f6086fc5cc4a56c8cf
Dylan <dylangill1921@gmail.com>
2025-02-23 20:34:32 -0500

    Delete week1 directory

--- week1/scripts/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-//IFE -Immediately Invoked Functional Expression
-(function() {
-    function Start()
-    {
-        console.log("Starting...");
-    }window.addEventListener("Load", Start);
-})();
